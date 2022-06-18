049005a1c33406e60349ff343714ddb18deda45c
MartinP96 <porenta.martin@gmail.com>
2022-06-18 23:54:28 +0200

    Added exception handling when reading configuration filter

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import WebBot
 
 if __name__ == '__main__':
 
-    scraper_filter_path = "configuration/avtonet_search_filter1.csv"
+    scraper_filter_path = "configuration/avtonet_search_filter.csv"
     scraper_output_path = "output/"
     bot1 = WebBot.WebBot(scraper_filter_path, scraper_output_path)
     bot1.run()
